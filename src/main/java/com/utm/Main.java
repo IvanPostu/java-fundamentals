b24a70a4da488c71418f8c168770f086bc12566d
IvanPostu <ipostu20000127@gmail.com>
2020-07-15 00:27:05 +0300

    Dependency log2j has been added to project.

--- src/main/java/com/utm/Main.java
+++ src/main/java/com/utm/Main.java
@@ -1,9 +1,15 @@
 package com.utm;
 
-class Main{
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+class Main {
+
+  static Logger logger = LogManager.getLogger(Main.class);
+
   public static void main(String...args){
     System.out.println("Hello");
-    
+    logger.warn("Hello");
   }
 }
 
